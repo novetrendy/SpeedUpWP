6fc1cc09a5464015b6142e40bc2d446999339967
novetrendy <oveckovaalena@novetrendy.eu>
2016-12-06 10:51:35 +0100

    Kompletní odstranění podpory komentářů

--- speed-up-wp.php
+++ speed-up-wp.php
@@ -3,7 +3,7 @@
 * Plugin Name: Speed Up WP !
 * Plugin URI: http://webstudionovetrendy.eu/
 * Description: This plugin make your Wordpress instalation much faster! Disable some WP featured for unlock more speed admin backend and also frontend. Use some techniques like JavaScript defer loading, remove some query string, remove not used widgets etc.
-* Version: 161128
+* Version: 161206
 * Text Domain: nt-speed-up-wp
 * Domain Path: /languages/
 * Author: Webstudio Nove Trendy
@@ -11,6 +11,8 @@
 * GitHub Plugin URI: https://github.com/novetrendy/SpeedUpWP
 * License: GPL2
 *** Changelog ***
+2016.12.06 - version 161206
+* Přidání kompletního odstranění komentářů - zatím bez podpory administrace (vyp/zap)
 2016.11.28 - version 161128
 * Přepsání pluginu kvůli E_NOTICE - všechny proměnné se testují isset()
 2016.11.27 - version 161127
@@ -139,6 +141,62 @@ add_filter( 'heartbeat_settings', 'optimize_heartbeat_settings' );
 function optimize_heartbeat_settings( $settings ) {$settings['autostart'] = false;$settings['interval'] = 60;return $settings;}
 function disable_heartbeat_unless_post_edit_screen() {global $pagenow;if ( $pagenow != 'post.php' && $pagenow != 'post-new.php' )wp_deregister_script('heartbeat');}
 }
+
+/* Remove comments */
+// Disable support for comments and trackbacks in post types
+function df_disable_comments_post_types_support() {
+	$post_types = get_post_types();
+	foreach ($post_types as $post_type) {
+		if(post_type_supports($post_type, 'comments')) {
+			remove_post_type_support($post_type, 'comments');
+			remove_post_type_support($post_type, 'trackbacks');
+		}
+	}
+}
+add_action('admin_init', 'df_disable_comments_post_types_support');
+
+// Close comments on the front-end
+function df_disable_comments_status() {
+	return false;
+}
+add_filter('comments_open', 'df_disable_comments_status', 20, 2);
+add_filter('pings_open', 'df_disable_comments_status', 20, 2);
+
+// Hide existing comments
+function df_disable_comments_hide_existing_comments($comments) {
+	$comments = array();
+	return $comments;
+}
+add_filter('comments_array', 'df_disable_comments_hide_existing_comments', 10, 2);
+
+// Remove comments page in menu
+function df_disable_comments_admin_menu() {
+	remove_menu_page('edit-comments.php');
+}
+add_action('admin_menu', 'df_disable_comments_admin_menu');
+
+// Redirect any user trying to access comments page
+function df_disable_comments_admin_menu_redirect() {
+	global $pagenow;
+	if ($pagenow === 'edit-comments.php') {
+		wp_redirect(admin_url()); exit;
+	}
+}
+add_action('admin_init', 'df_disable_comments_admin_menu_redirect');
+
+// Remove comments metabox from dashboard
+function df_disable_comments_dashboard() {
+	remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+}
+add_action('admin_init', 'df_disable_comments_dashboard');
+
+// Remove comments links from admin bar
+function df_disable_comments_admin_bar() {
+	if (is_admin_bar_showing()) {
+		remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
+	}
+}
+add_action('init', 'df_disable_comments_admin_bar');
 
 /* Disable oembed - since WP 4.4 */
 if ( isset ($sup_options['disable_oembed']) == 1) {
